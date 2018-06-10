acac3812d18a2b7b299491949008567df89cf3d8
Richard <rwphillis@gmail.com>
2018-06-11 07:23:29 +1000

    Simplify interface presented to user in "main"
    The display logic is very broken in this revision.

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,22 @@ use itertools::Itertools;
 /* Types */
 // Add a newtype for Disc
 #[derive(Debug, Clone)]
-pub struct Disc (u8);
+pub struct Disc(u8);
 
 impl Disc {
     pub fn new(disc: u8) -> Self {
         Disc(disc)
+    }
+}
+
+impl fmt::Display for Disc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // FIXME: Just a prototype
+        let max = 3;
+        let whitespace = (max - self.0) / 2;
+        let whitespace = (0..whitespace).map(|_| " ").collect::<String>();
+        let horiz_pad = (0..self.0 / 2).map(|_| "-").collect::<String>();
+        write!(f, "{}{}{}{}{}", whitespace, horiz_pad, self.0, horiz_pad, whitespace)
     }
 }
 
@@ -35,14 +46,14 @@ pub struct Peg {
 
 impl Peg {
     // Associated function (which constructs a Peg)
-    pub fn new(label: PegLabel, capacity: usize, largest_disc: Option<&Disc>) -> Self {
+    pub fn new(label: PegLabel, capacity: usize, largest_disc: Option<u8>) -> Self {
         // FIXME: I'm adding 1 to a user supplied int. If this int is maliciously chosen, this
         // could panic. Add a bounds check?
         match largest_disc {
             Some(i) => Peg {
                 label,
                 capacity,
-                stack: (0..i.0 + 1).rev().map(|x| Disc::new(x)).collect::<Vec<Disc>>(),
+                stack: (0..i + 1).rev().map(|x| Disc::new(x)).collect::<Vec<Disc>>(),
             },
             None => Peg {
                 label,
@@ -124,51 +135,57 @@ pub struct Board {
 
 impl Board {
     // Associated function (which constructs a `Board`)
-    fn new(largest_disc: &Disc) -> Self {
+    fn new(largest_disc: u8) -> Self {
         Board {
-            left: Peg::new(PegLabel::Left, (largest_disc.0 + 1) as usize, Some(&largest_disc)),
-            middle: Peg::new(PegLabel::Middle, (largest_disc.0 + 1) as usize, None),
-            right: Peg::new(PegLabel::Right, (largest_disc.0 + 1) as usize, None),
+            left: Peg::new(PegLabel::Left, (largest_disc + 1) as usize, Some(largest_disc)),
+            middle: Peg::new(PegLabel::Middle, (largest_disc + 1) as usize, None),
+            right: Peg::new(PegLabel::Right, (largest_disc + 1) as usize, None),
         }
     }
 }
 
 /* Functions */
-pub fn solve_game(disc: &Disc) -> Board {
+pub fn solve_game(disc_size: u8) -> Board {
     // Clear the terminal
     print!("{}[2J", 27 as char);
     let Board {
         mut left,
         mut middle,
         mut right,
-    } = Board::new(disc);
+    } = Board::new(disc_size);
 
     display_board(&left, &middle, &right);
 
-    move_tower(disc, &mut left, &mut middle, &mut right);
+    move_tower(disc_size, &mut left, &mut middle, &mut right);
+
+    display_board(&left, &middle, &right);
+
     Board { left, middle, right }
 }
 
 // Implements an approximation of the famous algorithm which solves the
 // "Towers of Hanoi" game using recursion. I've yet to determine the original
 // author of this bad boy.
-fn move_tower(disc: &Disc, source: &mut Peg, dest: &mut Peg, spare: &mut Peg) {
-    if disc.0 == 0 {
+fn move_tower(disc_size: u8, source: &mut Peg, dest: &mut Peg, spare: &mut Peg) {
+    if disc_size == 0 {
         if let Some(i) = source.stack.pop() {
+            display_board(source, dest, spare);
             dest.stack.push(i);
             display_board(source, dest, spare);
         } else {
             panic!("Unable to pop from \"source\" stack!");
         }
     } else {
-        move_tower(&Disc(disc.0 - 1), source, spare, dest);
+        move_tower(disc_size - 1, source, spare, dest);
         if let Some(i) = source.stack.pop() {
+            display_board(source, dest, spare);
             dest.stack.push(i);
             display_board(source, dest, spare);
         } else {
             panic!("Unable to pop from \"source\" stack!");
         }
-        move_tower(&Disc::new(disc.0 - 1), spare, dest, source);
+        move_tower(disc_size - 1, spare, dest, source);
+        display_board(source, dest, spare);
     }
 }
 
@@ -180,27 +197,27 @@ fn display_board(source: &Peg, dest: &Peg, spare: &Peg) {
     let (source, dest, spare) = (pegs[0], pegs[1], pegs[2]);
 
     // (l, m, r) means (left, middle, right)
-    for (l, m, r) in izip!(get_peg_display(&source).iter(),
-                           get_peg_display(&dest).iter(),
-                           get_peg_display(&spare).iter()) {
-        println!("{}     {}     {}", l, m, r);
+    for (l, m, r) in izip!(get_peg_representation(&source).iter(),
+                           get_peg_representation(&dest).iter(),
+                           get_peg_representation(&spare).iter()) {
+        println!("|{}|{}|{}|", l, m, r);
     }
     thread::sleep(time::Duration::from_millis(1000));
     // Clear the terminal
     print!("{}[2J", 27 as char);
 }
 
-fn get_peg_display(peg: &Peg) -> Vec<String> {
+fn get_peg_representation(peg: &Peg) -> Vec<String> {
     // Convert Vec<u8> to Iterator of Strings
     let discs = peg.stack.iter()
-                   .map(|x| format!("{}", x.0.to_string()))
-                   .rev();
+                         .map(|x| format!("{}", x))
+                         .rev();
     // Create the required amount of padding, and chain the `discs` iterator
     // of strings onto the end of this padding.
     // Note that I'm collecting into a Vec<String>, as attempting to return
     // the iterator yields a terribly long return type... FIXME
     (0..(peg.capacity - peg.stack.len()))
-        .map(|_| String::from("-"))
-        .chain(discs)
-        .collect::<Vec<_>>()
+                                 .map(|_| String::from("x"))
+                                 .chain(discs)
+                                 .collect::<Vec<_>>()
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use hanoi_simple::{Disc,solve_game};
 
 fn main() {
 
-    let solved_board = solve_game(&Disc::new(9));
+    let solved_board = solve_game(3);
 
-    println!("{}", solved_board.left);
-    println!("{}", solved_board.middle);
-    println!("{}", solved_board.right);
+    // println!("{}", solved_board.left);
+    // println!("{}", solved_board.middle);
+    // println!("{}", solved_board.right);
 }
